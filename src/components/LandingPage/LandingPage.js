da390b76af14c0cd4f414f2e7b3c6cfa3c3df9cb
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-07-30 14:42:44 +0500

    Redirect to login when no token

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./LandingPage.css";
-import { Link } from "react-router-dom";
+import { Link, Redirect, useHistory } from "react-router-dom";
 
 const LandingPage = () => {
+  useEffect(() => {
+    if (localStorage.getItem("userType") === null) {
+      console.log("type", localStorage.getItem("userType"));
+      history.push("/");
+    }
+  }, []);
+  const history = useHistory();
+  function handleClick() {
+    localStorage.getItem("userType") && localStorage.removeItem("userType");
+    history.push("/");
+  }
+
   return (
     <div className="landing-page">
       <h2>Name: {localStorage.getItem("name")}</h2>
       <p>Email: {localStorage.getItem("email")}</p>
       <p>User Type: {localStorage.getItem("userType")}</p>
       {localStorage.getItem("userType") === "admin" && (
-        <Link to="/signup">Sign Up</Link>
+        <Link to="/signup" style={{ margin: 8 }}>
+          Sign Up
+        </Link>
       )}
-      <div>
+      <div style={{ margin: 8 }}>
         {localStorage.getItem("userType") === "admin" && (
           <Link to="/add-affiliate">Add New Affiliate</Link>
         )}
+      </div>
+      <div>
+        <button
+          style={{ padding: 8, background: "lightblue", margin: 10 }}
+          onClick={handleClick}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
